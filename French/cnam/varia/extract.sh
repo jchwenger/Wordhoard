95b08aaf0e454531de7181caae80f918bb62006f
jchwenger <34098722+jchwenger@users.noreply.github.com>
2019-08-31 18:17:26 +0100

    cnam improvement in extract.sh, changes in name format, adding roots in utf8

--- French/cnam/varia/extract.sh
+++ French/cnam/varia/extract.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 echo "Computing and extracting the list of words ..."
-tail -n +59 ./donn* | iconv -f ISO8859-1 -t UTF8 | grep -o "^\S\+.*$" | sort | uniq > all-utf8.txt
-wc all-utf8.txt
-cat all-utf8.txt | grep -o "^\S\+." > all-words-utf8.txt
-wc all-words-utf8.txt
-cat all-words-utf8.txt | iconv -f UTF8 -t ASCII//TRANSLIT  | grep -o "^[a-z]\+" | sort | uniq > all-words-ascii.txt
-wc all-words-ascii.txt
-cat all-utf8.txt | iconv -f UTF8 -t ASCII//TRANSLIT  | sort | uniq > all-ascii.txt
-wc all-ascii.txt
-cat all-ascii.txt | awk '{ print $2 }' | grep -o "^[a-z]\+" | sort | uniq > all-root-ascii.txt
-wc all-root-ascii.txt
+tail -n +59 ./donn* | iconv -f ISO8859-1 -t UTF8 | grep -o "^\S\+.*$" | sort | uniq > cnam-info-utf8.txt
+wc -l cnam-info-utf8.txt
+
+cat cnam-info-utf8.txt | grep -o "^\S\+." > cnam-utf8.txt
+wc -l cnam-utf8.txt
+
+cat cnam-info-utf8.txt | awk '{ print $2 }' | sort | uniq > cnam-roots-utf8.txt
+wc -l cnam-roots-utf8.txt
+
+cat cnam-info-utf8.txt | iconv -f UTF8 -t ASCII//TRANSLIT  | sort | uniq > cnam-info-ascii.txt
+wc -l cnam-info-ascii.txt
+
+cat cnam-info-utf8.txt | iconv -f UTF8 -t ASCII//TRANSLIT  | grep -o "^[a-z]\+" | sort | uniq > cnam-ascii.txt
+wc -l cnam-ascii.txt
+
+cat cnam-info-utf8.txt | iconv -f UTF8 -t ASCII//TRANSLIT  | awk '{ print $2 }' | grep -o "^[a-z]\+" | sort | uniq > cnam-roots-ascii.txt
+wc -l cnam-roots-ascii.txt
 
